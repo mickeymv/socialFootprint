d2575f652ce7a6cf30cc4d7c2cc8bea56f879a24
Kedar Amrolkar <kedar.amrolkar@gmail.com>
2015-10-24 22:36:05 -0400

    made few changes

--- source/root/appslogin.php
+++ source/root/appslogin.php
@@ -5,33 +5,125 @@ session_start();
 if (!isset($_SESSION['username'])) {
 header('Location: index.php');
 }
-
-?>
-<html>
- <head>
-  <title>Apps login</title>
- </head>
- <body>
- <div>
- <p><?php echo "Current Loggedin User:".$_SESSION['username']?></p><p><a href="logout.php">Logout</a></p>
- </div>
-    <div> 
-	<h1>Facebook</h1>
-  	<?php 
-  	require_once __DIR__ . '/facebook-sdk-v5/autoload.php';
-  	$fb = new Facebook\Facebook([
+require_once __DIR__ . '/facebook-sdk-v5/autoload.php';
+$fb = new Facebook\Facebook([
   'app_id' => '1646244812311215',
   'app_secret' => 'b798f23b27dd787c88b69e4972fe8869',
   'default_graph_version' => 'v2.5',
   ]);
 
 $helper = $fb->getRedirectLoginHelper();
-$permissions = ['user_birthday', 'email', 'user_likes', 'user_friends', 'public_profile', 'user_hometown', 'user_location', 'user_about_me']; // optional
-$loginUrl = $helper->getLoginUrl('http://localhost/fblogin-callback.php', $permissions);
-
-echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
-
 ?>
-</div>
+<html>
+	<head>
+		<title>Apps login</title>
+	</head>
+ <body>
+	<div>
+		<p><?php echo "Current Loggedin User:".$_SESSION['username']?></p><p><a href="logout.php">Logout</a></p>
+	</div>
+    <div> 
+		<h3>Facebook</h3>
+<?php 
+	try{
+		$accessToken = $helper->getAccessToken();
+		
+		if (isset($accessToken)) {
+			$oAuth2Client = $fb->getOAuth2Client();  
+			$tokenMetadata = $oAuth2Client->debugToken($accessToken);  
+			$tokenMetadata->validateExpiration();   
+			if (! $accessToken->isLongLived()) {  
+				try {
+					$accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);  
+				}
+				catch (Facebook\Exceptions\FacebookSDKException $e) {  
+					echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>";  
+				} 
+				var_dump($accessToken->getValue());  
+			}	
+			$_SESSION['fb_access_token'] = (string) $accessToken;  
+			echo 'Login successful!   ';
+			$fb->setDefaultAccessToken($accessToken);
+
+			$response = $fb->get('/me');
+			$graphObject = $response->getGraphObject();
+			$uid = $graphObject['id'];
+			$name = $graphObject['name'];			
+			
+			$userNode = $fb->get('/me?fields=name,email,hometown,location,birthday,locale,picture,gender,languages')->getGraphUser();
+			$gender = $userNode['gender'];
+			$email = $userNode['email'];
+			$hometown = $userNode['hometown'];
+			$location = $userNode['location'];
+			$birthdate = $userNode['birthday']->format('Y/m/d');
+			$locale = $userNode['locale'];
+			$fbId = $userNode['id'];
+			$languageList = $userNode['languages'];
+			$languages = "";
+			foreach($languageList as $language)
+				$languages =  $languages.", ".$language['name'] .","; 
+			$profile_pic_url =  "http://graph.facebook.com/".$fbId."/picture";
+			$likes = $fb->get('/me/likes')->getGraphEdge();
+
+			echo "<img src=\"" . $profile_pic_url . "\" /><br>"; 
+			echo "Name: ".$name."<br>";
+			echo "Gender: " . $gender ."<br>"; 
+			echo "Email Id: " . $email ."<br>"; //what is the GET for email? not email/user_email
+			echo "Hometown is " . $hometown['name']."<br>"; //get for hometown?
+			echo "Current location is " . $location['name']."<br>"; //get for location?
+			echo "DOB" .$birthdate."<br>"; //get for dob? birthday? user_birthday?	
+			echo "Locale:" . $locale."<br>"; //??
+			echo "Languages: ".$languages;			
+			echo "<br>Likes: <br>"; 
+			echo '<table>';
+			foreach($likes as $like)
+			{
+				echo '<tr>';
+				foreach($like as $fields)
+				{
+					if(is_a($fields,'datetime') )
+						echo '<td>'.$fields->format('Y/m/d').'</td>';
+					else
+						echo '<td>'.$fields.'</td>';
+				}
+				echo '</tr>';
+			} 
+			echo '<table>';
+
+			//Save data to database
+			DEFINE('DB_USERNAME', 'root');
+			DEFINE('DB_PASSWORD', '');
+			DEFINE('DB_HOST', 'localhost');
+			DEFINE('DB_DATABASE', 'socialfootprint');
+
+			$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+			if ($conn->connect_error) {
+				die("couldn't connect to database server \n" . $conn->connect_error);
+			} 
+			try {
+				$query = "INSERT INTO facebookuser(fbId, name, email, gender, hometown, currentLocation, locale, languages, birthdate, profilePic) 
+							  VALUES ('". $fbId ."','".$name ."','".$email ."','".$gender ."','".$hometown ."','".$location ."','".$locale ."','".$languages ."','".$birthdate .
+							  "','".$profile_pic_url ."')";
+				$result = $conn->query($query) ;
+			} 
+			catch (Exception $e) {
+			}
+		}
+		else{
+			$permissions = ['user_birthday', 'email', 'user_likes', 'user_friends', 'public_profile', 'user_hometown', 'user_location', 'user_about_me']; // optional
+			$loginUrl = $helper->getLoginUrl('http://localhost/appslogin.php', $permissions);
+			echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+		}
+	} 
+	catch(Facebook\Exceptions\FacebookResponseException $e) {
+		echo 'Graph returned an error: ' . $e->getMessage();
+		exit;
+	} 
+	catch(Facebook\Exceptions\FacebookSDKException $e) {
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+?>
+	</div>
 </body>
 </html>
--- source/root/fblogin-callback.php
+++ source/root/fblogin-callback.php
@@ -29,31 +29,10 @@ try {
 }
 
 if (isset($accessToken)) {
-  // Logged in!
-  /*
-  echo '<h3>Access Token</h3>';  
-var_dump($accessToken->getValue()); 
- */
- 
- // The OAuth 2.0 client handler helps us manage access tokens  
 $oAuth2Client = $fb->getOAuth2Client();  
-
-// Get the access token metadata from /debug_token  
 $tokenMetadata = $oAuth2Client->debugToken($accessToken);  
-
-/*
-echo '<h3>Metadata</h3>';  
-var_dump($tokenMetadata);  
-  */
-  
-// Validation (these will throw FacebookSDKException's when they fail)  
-//$tokenMetadata->validateAppId($config['app_id']);  //This is taken from the fb example for login. What is $config?
-// If you know the user ID this access token belongs to, you can validate it here  
-// $tokenMetadata->validateUserId('123');  
 $tokenMetadata->validateExpiration();   
-   
 if (! $accessToken->isLongLived()) {  
-  // Exchanges a short-lived access token for a long-lived one  
   try {  
     $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);  
   } catch (Facebook\Exceptions\FacebookSDKException $e) {  
@@ -63,56 +42,15 @@ if (! $accessToken->isLongLived()) {
   echo '<h3>Long-lived</h3>';  
   var_dump($accessToken->getValue());  
 }
-
 $_SESSION['fb_access_token'] = (string) $accessToken;  
-  
-// User is logged in with a long-lived access token.  
-// You can redirect them to a members-only page.  
-// header('Location: https://example.com/members.php');
-
-  $_SESSION['facebook_access_token'] = (string) $accessToken;
-
-  // Now you can redirect to another page and use the
-  // access token from $_SESSION['facebook_access_token']
-  
-  echo 'Login success! <br>';
-  
-  // Sets the default fallback access token so we don't have to pass it to each request
+echo 'Login success! <br>';
 $fb->setDefaultAccessToken($accessToken);
-
 try {
   $response = $fb->get('/me');
   $graphObject = $response->getGraphObject();
-  
-/*
-  $userNode = $response->getGraphUser();
-echo '<br>Logged in as ' . $userNode->getName()."<br>";
-
-echo "<br>User's graphUser is " . $userNode."<br>";
-*/
-echo "<br>User's graphObject is " . $graphObject."<br>";
+  echo "<br>User's graphObject is " . $graphObject."<br>";
 echo "<br>User Name: ".$graphObject['name']."<br>";
 $uid = $graphObject['id'];
-/*
-GraphUser and GraphObject were returning same response when calling "/me"
-
-User's graphUser is {"name":"Mickey Vellukunnel","id":"10154230519659881"}
-
-User's graphObject is {"name":"Mickey Vellukunnel","id":"10154230519659881"}
-
-
-*/
-  
-  /* PHP SDK v5.0.0 */
-/* make the API call */
-/*
-$request = new Facebook\FacebookRequest(
-  $session,
-  'GET',
-  '/me'
-);
-*/
- //FacebookRequest? 
 $userNode = $fb->get('/me?fields=name,email,hometown,location,birthday,locale,picture,gender,languages')->getGraphUser();
 echo "<br>Your gender: " . $userNode['gender'] ."<br>"; 
 echo "<br>Your email Id: " . $userNode['email'] ."<br>"; //what is the GET for email? not email/user_email
@@ -123,8 +61,6 @@ echo "<br>Your current location is " . $userNode['location']['name']."<br>"; //g
 
 echo "<br>Your dob is " .$userNode['birthday']->format('d/m/Y')."<br>"; //get for dob? birthday? user_birthday?
 
-//echo "<br>Your about is " . $userNode['locale']."<br>"; //about? about_me? user_about_me?
-
 echo "<br>Your locale is " . $userNode['locale']."<br>"; //??
 
 echo "<br>Languages u know: ";
@@ -132,9 +68,9 @@ $languages = $userNode['languages'];
 foreach($languages as $language)
 	echo $language['name'] .","; 
 
-//echo "<br>Your pic is <br>" . $userNode['picture']."<br>"; 
+
 $profile_pic_url =  "http://graph.facebook.com/".$uid."/picture";
- //echo the image out
+
  echo "<br>your profile picture: <img src=\"" . $profile_pic_url . "\" /><br>"; 
 
 $likes = $fb->get('/me/likes')->getGraphEdge();
@@ -157,11 +93,11 @@ echo '<table>';
 
 
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
+
   echo 'Graph returned an error: ' . $e->getMessage();
   exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
+
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
   exit;
 }
