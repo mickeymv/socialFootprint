04e3c289938549d6d2481cd49acf7718de0ca389
Kedar Amrolkar <kedar.amrolkar@gmail.com>
2015-12-09 04:38:39 -0500

    adding google+ and wt calculation code

--- source/root/appslogin.php
+++ source/root/appslogin.php
@@ -24,9 +24,28 @@ $helper = $fb->getRedirectLoginHelper();
 			scope: r_emailaddress
 		</script>
 		<title>Apps login</title>
+		<meta name="google-signin-scope" content="profile email https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.me">
+		<meta name="google-signin-client_id" content="318733159872-p0249g76untk8tob74o888elp6u4r1m5.apps.googleusercontent.com">
+		<script src="https://apis.google.com/js/platform.js" async defer></script>
 	</head>
  <body>
 	<div>
+		<?php
+		$attributes= array();
+		$attributes['name'] = array();	$attributes['name']['visible'] = 0;		$attributes['name']['weight'] = 0.1; $attributes['name']['attr'] = 'name';
+		$attributes['gender'] = array();	$attributes['gender']['visible'] = 0;		$attributes['gender']['weight'] = 0.1;	 $attributes['gender']['attr'] = 'gender';
+		$attributes['hometown'] = array();	$attributes['hometown']['visible'] = 0;		$attributes['hometown']['weight'] = 0.1; $attributes['hometown']['attr'] = 'hometown';
+		$attributes['currentlocation'] = array();	$attributes['currentlocation']['visible'] = 0;		$attributes['currentlocation']['weight'] = 0.1; $attributes['currentlocation']['attr'] = 'currentlocation';
+		$attributes['dob'] = array();	$attributes['dob']['visible'] = 0;		$attributes['dob']['weight'] = 0.1; $attributes['dob']['attr'] = 'dob';
+		$attributes['language'] = array();	$attributes['language']['visible'] = 0;		$attributes['language']['weight'] = 0.1; $attributes['language']['attr'] = 'language';
+		$attributes['checkins'] = array();	$attributes['checkins']['visible'] = 0;		$attributes['checkins']['weight'] = 0.1; $attributes['checkins']['attr'] = 'checkins';
+		$attributes['nickname'] = array();	$attributes['nickname']['visible'] = 0;		$attributes['nickname']['weight'] = 0.1; $attributes['nickname']['attr'] = 'nickname';
+		$attributes['relationshipstatus'] = array();	$attributes['relationshipstatus']['visible'] = 0;		$attributes['relationshipstatus']['weight'] = 0.1; $attributes['relationshipstatus']['attr'] = 'relationshipstatus';
+		$attributes['industry'] = array();	$attributes['industry']['visible'] = 0;		$attributes['industry']['weight'] = 0.1; $attributes['industry']['attr'] = 'industry';
+		$attributes['email'] = array();	$attributes['email']['visible'] = 0;		$attributes['email']['weight'] = 0.1; $attributes['email']['attr'] = 'email';
+		$attributes['status'] = array();	$attributes['status']['visible'] = 0;		$attributes['status']['weight'] = 0.1; $attributes['status']['attr'] = 'status';
+		echo '<script type="text/javascript">var attributes = '.json_encode($attributes)."</script>"?>
+
 		<span><?php echo "<b>Current User: </b>".$_SESSION['username']?></span>
 		&nbsp;&nbsp;&nbsp;&nbsp;
 		<span>
@@ -86,17 +105,25 @@ $helper = $fb->getRedirectLoginHelper();
 						$graphObject = $response->getGraphObject();
 						$uid = $graphObject['id'];
 						$name = $graphObject['name'];			
+						$attributes['name']['visible'] = 1;
 						
+						$text =" ";
 						$userNode = $fb->get('/me?fields=name,email,hometown,location,birthday,locale,picture,gender,languages,link')->getGraphUser();
-						$gender = (isset($userNode['gender']))?$userNode['gender']:'HIDDEN';
-						$email = (isset($userNode['email']))?$userNode['email']:'HIDDEN';
-						$hometown =  (isset($userNode['hometown']))?$userNode['hometown']['name']:'HIDDEN';
-						$location =  (isset($userNode['location']))?$userNode['location']['name']:'HIDDEN';
-						$birthdate =  (isset($userNode['birthday']))?$userNode['birthday']->format('Y/m/d'):'HIDDEN';
-						$locale = (isset($userNode['locale']))?$userNode['locale']:'HIDDEN';
+						$text.= (isset($userNode['gender']))? 'attributes.gender.visible=1;':'';
+						$gender = (isset($userNode['gender']))? ($userNode['gender']):'HIDDEN';
+						$text.= (isset($userNode['email']))? 'attributes.email.visible = 1;' :'';
+						$email = (isset($userNode['email']))? ($userNode['email']):'HIDDEN';
+						$text.=  (isset($userNode['hometown']))? 'attributes.hometown.visible = 1;':'';
+						$hometown =  (isset($userNode['hometown']))? ($userNode['hometown']['name']):'HIDDEN';
+						$text.=  (isset($userNode['location']))? 'attributes.currentlocation.visible = 1;':'';
+						$location =  (isset($userNode['location']))? ($userNode['location']['name']):'HIDDEN';
+						$text.=  (isset($userNode['birthday']))? 'attributes.dob.visible = 1;':'';
+						$birthdate =  (isset($userNode['birthday']))? ($userNode['birthday']->format('Y/m/d')):'HIDDEN';
+						
 						$fbId = $userNode['id'];
 						$link = $userNode['link'];
-						$languageList = (isset($userNode['languages']))?$userNode['languages']:NULL;
+						$text.= (isset($userNode['languages']))?'attributes.language.visible = 1;':'';
+						$languageList = (isset($userNode['languages']))? ($userNode['languages']):NULL;
 						$languages = "";
 						if($languageList!=NULL)
 							foreach($languageList as $language)
@@ -109,19 +136,20 @@ $helper = $fb->getRedirectLoginHelper();
 
 						echo "<img alt='Profile Pic' style='width:100px;height:100px;' src=\"" . $profile_pic_url . "\" >";
 						echo " <a href=\"" .$link . "\">Profile URL</a>";
-						
-						$response = $fb->get('/me?fields=birthday,feed.with(location),events');
+					
+						$response = $fb->get('/me?fields=birthday,events');
 						$graphObject = $response->getGraphObject();
-						$checkins = (isset($graphObject['feed']))?$graphObject['feed']:NULL;
-						$events = (isset($graphObject['events']))?$graphObject['events']:NULL;
-
+						$text.= (isset($graphObject['feed']))? 'attributes.checkins.visible = 1;':'';
+						$checkins = (isset($graphObject['feed']))? ($graphObject['feed']):NULL;
+						
+						echo '<script type="text/javascript">'.$text.'console.log(attributes)</script>';
 						echo "<br><b>Name: </b>".$name."<br>";
 						echo "<b>Gender: </b>" . $gender ."<br>"; 
 						echo "<b>Email Id: </b>" . $email ."<br>"; 
 						echo "<b>Hometown: </b>" . $hometown."<br>"; 
 						echo "<b>Current location: </b>" . $location."<br>";
 						echo "<b>DOB: </b>" .$birthdate."<br>";
-						echo "<b>Locale: </b>" . $locale."<br>"; 
+						//echo "<b>Locale: </b>" . $locale."<br>"; 
 						echo "<b>Languages: </b>".$languages."<br>";	
 						echo "<b>Checkins: </b><br>"; 
 						if($checkins!=NULL)
@@ -160,7 +188,7 @@ $helper = $fb->getRedirectLoginHelper();
 						} 
 						try {
 							$query = "INSERT INTO facebookuser(fbId, name, email, gender, hometown, currentLocation, locale, languages, birthdate, profilePic) 
-										  VALUES ('". $fbId ."','".$name ."','".$email ."','".$gender ."','".$hometown ."','".$location ."','".$locale ."','".$languages ."','".$birthdate .
+										  VALUES ('". $fbId ."','".$name ."','".$email ."','".$gender ."','".$hometown ."','".$location ."','jjj','".$languages ."','".$birthdate .
 											  "','".$profile_pic_url ."')";
 							$result = $conn->query($query) ;
 						} 
@@ -190,9 +218,110 @@ $helper = $fb->getRedirectLoginHelper();
 				</div>
 			</div>
 		</div>
+		<div id="three"> 
+			<h3>Google+</h3>
+			<div>
+				<div id='gprofileLogin' class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+				<div id='gprofile' style="visibility: hidden">
+					<img src="" id="gProfilePic" alt="Profile Pic" style="width:100px;height:100px;">
+					<span id="gProfileURL"></span>
+					<div><b>Name: </b><span id='gFirstName'></span></div>
+					<div><b>NickName: </b><span id='gnickname'></span></div>
+					<div><b>birthday: </b><span id='gbirthday'></span></div>
+					<div><b>Gender: </b><span id='ggender'></span></div>
+					<div><b>Email: </b><span id='gEmailAddress'></span></div>
+					<div><b>Current Location: </b><span id='gcurrentLocation'></span></div>
+					<div><b>Language: </b><span id='glanguage'></span></div>
+					<div><b>Places Lived: </b><span id='gplacesLived'></span></div>
+					<div><b>Relationship Status: </b><span id='grelationshipStatus'></span></div>
+					<div id="Positionsdiv" style="visibility: hidden"><b>Work: </b><table border="1"><tbody id="Positions"></tbody></table></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div>
+	<h3><button onclick="onCalculateClick()">Calculate</button><span id="tw" style="visibility: hidden"><b> Total Weight = <span id='totalweight'></span></b><span></h3>
+	<table border="1" id='wtt' style="visibility: hidden"><tbody id="weightTable"></tbody></table>
+	
 	</div>
 	<!-- Faceook scripts -->
 	<script type="text/javascript">
+			function onCalculateClick() {
+				document.getElementById("wtt").style.visibility='visible';
+				document.getElementById("tw").style.visibility='visible';
+				var totalWeight=0;
+				var tbody = document.getElementById('weightTable');
+				tbody.innerHTML = '';
+				th = "<tr><th>Attribute</th><th>Visible?</th><th>Weight</th</tr>"
+				tbody.innerHTML += th;
+				for  (var key in attributes)
+				{
+					var tr = "<tr>";
+					tr += "<td>" + attributes[key].attr + "</td><td>" + attributes[key].visible + "</td><td>" + attributes[key].weight + "</td></tr>";
+					totalWeight += attributes[key].visible*attributes[key].weight;
+					tbody.innerHTML += tr;
+				}
+				document.getElementById('totalweight').innerHTML = totalWeight.toFixed(2);
+			}
+
+			function onSignIn(googleUser) {
+				// Useful data for your client-side scripts:
+
+				var data = googleUser.getBasicProfile();
+				var id_token = googleUser.getAuthResponse().id_token;
+				console.log("ID Token: " + id_token);
+				
+				document.getElementById("gprofileLogin").setAttribute("style","height:0px");
+				document.getElementById("gprofile").style.visibility='visible';
+				document.getElementById("gprofileLogin").style.visibility='hidden';
+				document.getElementById("gFirstName").innerHTML = data.getName();		attributes.name.visible = 1;
+				document.getElementById("gEmailAddress").innerHTML = data.getEmail();	attributes.email.visible = 1;
+				document.getElementById("gProfilePic").src = data.getImageUrl();
+
+				var apikey="AIzaSyCKXsT-m5yTZo3Ki0GecHfUBpa-L0WOUbk";
+				var url = "https://www.googleapis.com/plus/v1/people/"+data.getId()+"?fields=ageRange%2Cbirthday%2CcurrentLocation%2Cgender%2Clanguage%2Cnickname%2CplacesLived%2CrelationshipStatus%2Curl&key="+ apikey;
+				var representationOfDesiredState = "The";
+				var client = new XMLHttpRequest();
+				client.open("GET", url, false);
+				client.send(representationOfDesiredState);
+				console.log(client.responseText);
+				var userData = JSON.parse(client.responseText);
+
+				var aggender = (userData.gender != undefined)?userData.gender:'HIDDEN';
+				var agbirthday = (userData.birthday != undefined)?userData.birthday:'HIDDEN';
+				var agcurrentLocation = (userData.currentLocation != undefined)?userData.currentLocation:'HIDDEN';
+				var aglanguage = (userData.language != undefined)? userData.language:'HIDDEN';
+				var agnickname = (userData.nickname != undefined)?userData.nickname:'HIDDEN';
+				var agplacesLived = (userData.placesLived != undefined)?userData.placesLived[0].value:'HIDDEN';
+				var agrelationshipStatus = (userData.relationshipStatus != undefined)?userData.relationshipStatus:'HIDDEN';
+
+				if (userData.url != undefined)
+					document.getElementById("gProfileURL").innerHTML = "<a href='" + userData.url + "'>Profile URL</a>";
+				
+				document.getElementById("ggender").innerHTML = aggender;	
+				if(aggender !='HIDDEN')
+					attributes.gender.visible = 1;
+				document.getElementById("gbirthday").innerHTML = agbirthday;	
+				if(agbirthday !='HIDDEN')
+					attributes.dob.visible = 1;
+				document.getElementById("gcurrentLocation").innerHTML = agcurrentLocation;	 
+				if(agcurrentLocation !='HIDDEN')
+					attributes.currentlocation.visible = 1;
+				document.getElementById("glanguage").innerHTML = aglanguage;	
+				console.log(aglanguage);
+				if(aglanguage !='HIDDEN')
+					attributes.language.visible = 1;
+				document.getElementById("gnickname").innerHTML =agnickname;	
+				if(agnickname !='HIDDEN')
+					attributes.nickname.visible = 1;
+				document.getElementById("gplacesLived").innerHTML = agplacesLived;	
+				if(agplacesLived !='HIDDEN')
+					attributes.checkins.visible = 1;
+				document.getElementById("grelationshipStatus").innerHTML = agrelationshipStatus;	
+				if(agrelationshipStatus !='HIDDEN')
+					attributes.relationshipstatus.visible = 1;
+			};
+
 		    // Setup an event listener to make an API call once auth is complete
 			function onLinkedInLoad() {
 				IN.Event.on(IN, "auth", getProfileData);
@@ -200,6 +329,11 @@ $helper = $fb->getRedirectLoginHelper();
 
 			function onLogoutClick()
 			{
+				var auth2 = gapi.auth2.getAuthInstance();
+				auth2.signOut().then(function () {
+					console.log('google+ User signed out.');
+				});
+
 				console.log("logging out");
 				IN.User.logout(afterLogout);
 			}
@@ -215,12 +349,22 @@ $helper = $fb->getRedirectLoginHelper();
 				document.getElementById("profile").style.visibility='visible';
 				document.getElementById("FirstName").innerHTML = data.firstName;
 				document.getElementById("LastName").innerHTML = data.lastName;
+				attributes.name.visible = 1;
 				document.getElementById("headline").innerHTML = data.headline;
+				if(data.headline != undefined || data.headline !='')
+					attributes.status.visible = 1;
+
 				document.getElementById("UserId").innerHTML = data.id;			
 				document.getElementById("ProfilePic").src = data.pictureUrl;	
 				document.getElementById("Location").innerHTML = data.location.name;
+				if(data.location.name != undefined && data.location.name !='')
+					attributes.currentlocation.visible = 1;
 				document.getElementById("Industry").innerHTML = data.industry;
+				if(data.industry != undefined && data.industry !='')
+					attributes.industry.visible = 1;
 				document.getElementById("Summary").innerHTML = data.summary;
+				if(data.summary != undefined && data.summary!='')
+					attributes.status.visible = 1;
 				obj = data.positions;
 				if(obj._total != 0)
 					document.getElementById("Positionsdiv").style.visibility='visible'
@@ -233,7 +377,9 @@ $helper = $fb->getRedirectLoginHelper();
 					tbody.innerHTML += tr;
 				}
 				document.getElementById("ProfileURL").innerHTML = "<a href='" + data.publicProfileUrl + "'>Profile URL</a>";
-				document.getElementById("EmailAddress").innerHTML = data.emailAddress;
+				document.getElementById("EmailAddress").innerHTML = data.emailAddress;   
+				if(data.emailAddress != undefined && data.emailAddress !='')
+					attributes.email.visible = 1;
 			}
 
 			// Handle an error response from the API call
@@ -245,7 +391,7 @@ $helper = $fb->getRedirectLoginHelper();
 			function getProfileData() {
 				IN.API.Raw("/people/~:(id,firstName,lastName,headline,picture-url,location,industry,summary,positions,public-profile-url,email-address)?format=json").result(onSuccess).error(onError);
 			}
-
+console.log(attributes);
 		</script>
-</body>
+	</body>
 </html>
